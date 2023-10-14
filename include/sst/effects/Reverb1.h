surge-synthesizer/sst-effects

commit 3e0b59f27176841ce54486879496f391ca0ab2bb

Allow Reverb1 Room Size to Modulate (#16)
It always cou,d just it cleared a buffer when you did improperly. So
now it doesn't clear that buffer any more.

include/sst/effects/Reverb1.h
@@ -314,9 +314,10 @@ inline void Reverb1<FXConfig>::processBlock(float *__restrict dataL, float *__re
 
 template <typename FXConfig> inline void Reverb1<FXConfig>::loadpreset(int id)
 {
-    shape = id;
+    if (shape != id)
+        clear_buffers();
 
-    clear_buffers();
+    shape = id;
 
     switch (id)
     {

commit 90dd21829fb3e2b57999f03d4f2bde45d64a0b87

Clear reverb buffers on re-initialize
This stops an occasionaly noise-on-patch-switch which
was caused by 3e0b59f27176841ce54486879496f391ca0ab2bb

include/sst/effects/Reverb1.h
@@ -196,6 +196,8 @@ template <typename FXConfig> inline void Reverb1<FXConfig>::initialize()
         delay_pan_R[t] = sqrt(0.5 + 0.495 * xbp);
     }
     delay_pos = 0;
+
+    clear_buffers();
 }
 
 template <typename FXConfig>