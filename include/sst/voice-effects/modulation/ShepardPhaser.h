26670d29803cb0c40f80b1daf614c06c07bd3101
Andreya-Autumn <andreya.ek.frisk@gmail.com>
2024-08-28 03:06:33 +0200

    checkpoint two

--- include/sst/voice-effects/modulation/ShepardPhaser.h
+++ include/sst/voice-effects/modulation/ShepardPhaser.h
@@ -25,6 +25,7 @@
 #include "../VoiceEffectCore.h"
 
 #include <iostream>
+#include <cmath>
 
 #include "sst/basic-blocks/mechanics/block-ops.h"
 #include "sst/basic-blocks/modulators/SimpleLFO.h"
@@ -35,16 +36,13 @@ template <typename VFXConfig> struct ShepardPhaser : core::VoiceEffectTemplateBa
 {
     static constexpr const char *effectName{"Shepard Phaser"};
 
-    static constexpr int numFloatParams{5};
-    static constexpr int numIntParams{1};
-
-    static constexpr int maxPhases{6};
+    static constexpr int numFloatParams{4};
+    static constexpr int numIntParams{2};
 
     basic_blocks::dsp::RNG rng;
 
     enum FloatParams
     {
-        fpFeedback,
         fpResonance,
         fpRate,
         fpStartFreq,
@@ -53,8 +51,8 @@ template <typename VFXConfig> struct ShepardPhaser : core::VoiceEffectTemplateBa
 
     enum IntParams
     {
-        ipMode,
-        ipStereo
+        ipStereo,
+        ipPeaks
     };
 
     ShepardPhaser() : core::VoiceEffectTemplateBase<VFXConfig>() {}
@@ -67,16 +65,14 @@ template <typename VFXConfig> struct ShepardPhaser : core::VoiceEffectTemplateBa
 
         switch (idx)
         {
-        case fpFeedback:
-                return pmd().asPercent().withDefault(0.5f).withName("Feedback");
         case fpResonance:
-            return pmd().asPercent().withDefault(0.707).withName("Resonance");
+            return pmd().asPercent().withDefault(1.0f).withName("Resonance");
         case fpRate:
-            return pmd().asLfoRate(-7, 2).withName("Rate");
+            return pmd().asLfoRate(-7, 0).withDefault(-3).withName("Rate");
         case fpStartFreq:
-            return pmd().asAudibleFrequency().withName("Start Freq").withDefault(-20);
+            return pmd().asAudibleFrequency().withName("Start Freq").withDefault(-30);
         case fpEndFreq:
-            return pmd().asAudibleFrequency().withName("End Freq").withDefault(20);
+            return pmd().asAudibleFrequency().withName("End Freq").withDefault(40);
         }
         return pmd().asFloat().withName("Error");
     }
@@ -85,140 +81,117 @@ template <typename VFXConfig> struct ShepardPhaser : core::VoiceEffectTemplateBa
     {
         using pmd = basic_blocks::params::ParamMetaData;
         using md = sst::filters::CytomicSVF::Mode;
-        
+
         switch (idx)
         {
-        case ipMode:
-            return pmd()
-                .asBool()
-                .withName("Mode")
-                .withUnorderedMapFormatting({
-                    {md::ALL, "AP"},
-                    {md::BP, "BP"},
-                })
-                .withDefault(md::ALL);
         case ipStereo:
             return pmd().asBool().withDefault(false).withName("Stereo");
+        case ipPeaks:
+            return pmd().asInt().withRange(4, 12).withDefault(8).withName("Peaks");
         }
         return pmd().asInt().withName("Error");
     }
 
-    void initVoiceEffect()
-    {
-//        lipolFb.set_target_instant(
-//            std::sqrt(std::clamp(this->getFloatParam(fpFeedback), 0.f, 1.f)));
-    }
+    void initVoiceEffect() {}
 
     void initVoiceEffectParams() { this->initToParamMetadataDefault(this); }
-     
-    void calc_coeffs(const float freq, const int filterIndex)
-    {
-
-    }
-    
 
     using lfo_t = sst::basic_blocks::modulators::SimpleLFO<ShepardPhaser, VFXConfig::blockSize>;
-    lfo_t sawLFO[8]{this, this, this, this, this, this, this, this};
-    lfo_t triLFO[8]{this, this, this, this, this, this, this, this};
-    
-    
+    lfo_t phasor{this};
+
+    float triangle(const float &p)
+    {
+        auto res = 0.f;
+        if (p < 0.5)
+        {
+            res = 4 * p - 1.f;
+        }
+        else
+        {
+            res = (1.f - 4 * (p - 0.5));
+        }
+        return res * .5 + .5;
+    }
+
     void processStereo(float *datainL, float *datainR, float *dataoutL, float *dataoutR,
                        float pitch)
     {
         auto lfoRate = this->getFloatParam(fpRate);
         auto range = this->getFloatParam(fpEndFreq) - this->getFloatParam(fpStartFreq);
-        auto res = std::clamp(this->getFloatParam(fpResonance), 0.f, 1.f);
-        auto mode = (sst::filters::CytomicSVF::Mode)this->getIntParam(ipMode);
-        
+        auto res = std::clamp(this->getFloatParam(fpResonance) * .2f + .8f, 0.f, 1.0f);
+        int peaks = this->getIntParam(ipPeaks);
+
         namespace mech = sst::basic_blocks::mechanics;
-        
+
         if (isFirst)
         {
-            for (int i = 0; i < 8; ++i)
-            {
-                float sawOffset = i / 8.f;
-                float triOffset = sawOffset + .75f;
-                sawLFO[i].applyPhaseOffset(sawOffset);
-                triLFO[i].applyPhaseOffset(triOffset);
-            }
+            phasor.attack(lfo_t::RAMP);
             isFirst = false;
         }
-        
+
         mech::clear_block<VFXConfig::blockSize>(dataoutL);
         mech::clear_block<VFXConfig::blockSize>(dataoutR);
-                
-        for (int i = 0; i < 8; ++i)
+
+        phasor.process_block(lfoRate, 0.f, lfo_t::RAMP);
+        auto phasorValue = phasor.lastTarget * .5f + .5f;
+
+        for (int i = 0; i < peaks; ++i)
         {
-            sawLFO[i].process_block(lfoRate, 0.f, lfo_t::RAMP);
-            auto freqMod = this->getFloatParam(fpStartFreq) + (range * (sawLFO[i].lastTarget * .5f + .5f));
-            // set filter to it
+
+            float iPhase = phasorValue + (i / static_cast<float>(peaks));
+            if (iPhase > 1)
+            {
+                iPhase -= 1;
+            }
+            float iTri = triangle(iPhase);
+
+            auto freqMod = this->getFloatParam(fpStartFreq) + (range * iPhase);
             auto freq = 440.f * this->note_to_pitch_ignoring_tuning(freqMod);
-            filters[i].template setCoeffForBlock<VFXConfig::blockSize>(mode, freq, freq, res, res, this->getSampleRateInv(), 1.f, 1.f);
-            
+            filters[i].template setCoeffForBlock<VFXConfig::blockSize>(
+                sst::filters::CytomicSVF::Mode::BP, freq, freq, res, res, this->getSampleRateInv(),
+                1.f, 1.f);
+
             float tmpL alignas(16)[VFXConfig::blockSize];
             float tmpR alignas(16)[VFXConfig::blockSize];
-            
-            filters[i].template processBlock<VFXConfig::blockSize>(datainL, datainR, tmpL, tmpR);
-            
-            triLFO[i].process_block(lfoRate, .707f, lfo_t::TRI);
-            auto levelMod = triLFO[i].lastTarget * .5f + .5f;
-            levelMod = levelMod * levelMod * levelMod;
-            lipolLevel[i].set_target(levelMod);
-            lipolLevel[i].multiply_2_blocks(tmpL, tmpR);
-            
-            mech::scale_accumulate_from_to<VFXConfig::blockSize>(tmpL, tmpR, 0.3333f, dataoutL, dataoutR);
-        }
-    }
-    
-    /*
-     Feedback stuff
-    {
-    
-        sst::basic_blocks::mechanics::copy_from_to<VFXConfig::blockSize>(datainL, dataoutL);
-        sst::basic_blocks::mechanics::copy_from_to<VFXConfig::blockSize>(datainR, dataoutR);
-        this->lipolFb.set_target(
-            std::sqrt(std::clamp(this->getFloatParam(this->fpFeedback), 0.f, 0.97f)));
-        float fb alignas(16)[VFXConfig::blockSize];
-        this->lipolFb.store_block(fb);
+            mech::copy_from_to<VFXConfig::blockSize>(datainL, tmpL);
+            mech::copy_from_to<VFXConfig::blockSize>(datainR, tmpR);
 
-        for (int k = 0; k < VFXConfig::blockSize; ++k)
-        {
-            float sumL = 0.f;
-            float sumR = 0.f;
-            
-            float dL[4];
-            float dR[4];
-            
-            for (int i = 0; i < 4; ++i)
+            for (int k = 0; k < VFXConfig::blockSize; ++k)
             {
-
-                
-                this->filters[i].processBlockStep(dL[i], dR[i]);
-                
-
-                
-                dL[i] *= triLFO[i].lastTarget * .5f + .5f;
-                dR[i] *= triLFO[i].lastTarget * .5f + .5f;
-                
-                sumL += dL[i];
-                sumR += dR[i];
+                filters[i].processBlockStep(tmpL[k], tmpR[k]);
+                softClip(tmpL[k], tmpR[k]);
             }
-            dataoutL[k] = dL[0];
-            dataoutR[k] = dR[0];
-            
-            dataoutL[k] = sumL;
-            dataoutR[k] = sumR;
+
+            lipolLevel[i].set_target(iTri * iTri * iTri);
+            lipolLevel[i].multiply_2_blocks(tmpL, tmpR);
+
+            mech::scale_accumulate_from_to<VFXConfig::blockSize>(tmpL, tmpR, 0.3333f, dataoutL,
+                                                                 dataoutR);
         }
     }
-     */
 
-protected:
+    void softClip(float &L, float &R)
+    {
+        L = std::clamp(L, -1.5f, 1.5f);
+        L = L - 4.0 / 27.0 * L * L * L;
+
+        R = std::clamp(R, -1.5f, 1.5f);
+        R = R - 4.0 / 27.0 * R * R * R;
+    }
+
+    void softClip(float &C)
+    {
+        C = std::clamp(C, -1.5f, 1.5f);
+        C = C - 4.0 / 27.0 * C * C * C;
+    }
+
+  protected:
     std::array<float, numFloatParams> mLastParam{};
     std::array<int, numIntParams> mLastIParam{};
-     std::array<sst::filters::CytomicSVF, 8> filters;
-    // float fbAmt[2][4];
-//    sst::basic_blocks::dsp::lipol_sse<VFXConfig::blockSize, true> lipolFb;
-    sst::basic_blocks::dsp::lipol_sse<VFXConfig::blockSize, true> lipolLevel[8];
+
+    std::array<sst::filters::CytomicSVF, 12> filters;
+
+    sst::basic_blocks::dsp::lipol_sse<VFXConfig::blockSize, true> lipolLevel[12];
     bool isFirst{true};
 };
 } // namespace sst::voice_effects::modulation
