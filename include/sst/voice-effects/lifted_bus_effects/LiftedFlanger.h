0ede944e9ecb87a484b21736801545ff5705243b
Paul <baconpaul@users.noreply.github.com>
2024-09-10 11:06:16 -0400

    "Lift" bus effects into the voice effect space (#129)
    This commit starts giving us the tools needed to
    "lift" a bus effect into a voice effect, with all the
    pros and cons that entails. Including
    
    - New memroy pool API
    - Lift of Delay, R1 and 2, and flanger
    - Variety of associated other changes
    
    A few things don't work well now
    
    - Delay tail calculation is garbage. Flanger is unchecked
    - Temposync support not working yet.

---
+++ include/sst/voice-effects/lifted_bus_effects/LiftedFlanger.h
@@ -0,0 +1,91 @@
+/*
+ * sst-effects - an open source library of audio effects
+ * built by Surge Synth Team.
+ *
+ * Copyright 2018-2023, various authors, as described in the GitHub
+ * transaction log.
+ *
+ * sst-effects is released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * The majority of these effects at initiation were factored from
+ * Surge XT, and so git history prior to April 2023 is found in the
+ * surge repo, https://github.com/surge-synthesizer/surge
+ *
+ * All source in sst-effects available at
+ * https://github.com/surge-synthesizer/sst-effects
+ */
+
+#ifndef INCLUDE_SST_VOICE_EFFECTS_LIFTED_BUS_EFFECTS_LIFTEDFLANGER_H
+#define INCLUDE_SST_VOICE_EFFECTS_LIFTED_BUS_EFFECTS_LIFTEDFLANGER_H
+
+#include "../VoiceEffectCore.h"
+#include "sst/effects/Flanger.h"
+#include "sst/basic-blocks/params/ParamMetadata.h"
+#include "sst/basic-blocks/mechanics/block-ops.h"
+#include "FXConfigFromVFXConfig.h"
+
+namespace sst::voice_effects::liftbus
+{
+
+namespace mech = sst::basic_blocks::mechanics;
+template <typename VFXConfig> struct LiftedFlanger : core::VoiceEffectTemplateBase<VFXConfig>
+{
+    static constexpr const char *effectName{"Reverb1"};
+
+    static constexpr int numFloatParams{7};
+    static constexpr int numIntParams{2};
+
+    using flanger_t =
+        sst::effects::flanger::Flanger<FXConfigFromVFXConfig<LiftedFlanger<VFXConfig>>>;
+    LiftHelper<LiftedFlanger, flanger_t> helper;
+
+    LiftedFlanger() : helper(this), core::VoiceEffectTemplateBase<VFXConfig>() {}
+
+    // turns out the two ints are first making this easy
+    basic_blocks::params::ParamMetaData paramAt(int idx) const
+    {
+        using pmd = basic_blocks::params::ParamMetaData;
+
+        return helper.busFX->paramAt(idx + 2);
+    }
+
+    basic_blocks::params::ParamMetaData intParamAt(int idx) const
+    {
+        return helper.busFX->paramAt(idx);
+    }
+
+    void initVoiceEffect() { helper.init(); }
+
+    void initVoiceEffectParams() { this->initToParamMetadataDefault(this); }
+
+    size_t tailLength() const { return helper.busFX->getRingoutDecay() * VFXConfig::blockSize; }
+
+    void setupValues()
+    {
+        for (int i = 0; i < numFloatParams; ++i)
+        {
+            helper.valuesForFX[i + 2] = this->getFloatParam(i);
+        }
+        for (int i = 0; i < numIntParams; ++i)
+        {
+            helper.valuesForFX[i] = this->getIntParam(i);
+        }
+
+        helper.valuesForFX[flanger_t::fl_mix] = 1.0;
+        helper.valuesForFX[flanger_t::fl_width] = helper.busFX->getDefaultWidth();
+    }
+
+    void processStereo(float *datainL, float *datainR, float *dataoutL, float *dataoutR,
+                       float pitch)
+    {
+        setupValues();
+        mech::copy_from_to<VFXConfig::blockSize>(datainL, dataoutL);
+        mech::copy_from_to<VFXConfig::blockSize>(datainR, dataoutR);
+        helper.busFX->processBlock(dataoutL, dataoutR);
+    }
+};
+} // namespace sst::voice_effects::liftbus
+#endif // SHORTCIRCUITXT_LIFTEDREVERB2_H
---
+++ include/sst/voice-effects/lifted_bus_effects/LiftedReverb1.h
@@ -0,0 +1,91 @@
+/*
+ * sst-effects - an open source library of audio effects
+ * built by Surge Synth Team.
+ *
+ * Copyright 2018-2023, various authors, as described in the GitHub
+ * transaction log.
+ *
+ * sst-effects is released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * The majority of these effects at initiation were factored from
+ * Surge XT, and so git history prior to April 2023 is found in the
+ * surge repo, https://github.com/surge-synthesizer/surge
+ *
+ * All source in sst-effects available at
+ * https://github.com/surge-synthesizer/sst-effects
+ */
+
+#ifndef INCLUDE_SST_VOICE_EFFECTS_LIFTED_BUS_EFFECTS_LIFTEDREVERB1_H
+#define INCLUDE_SST_VOICE_EFFECTS_LIFTED_BUS_EFFECTS_LIFTEDREVERB1_H
+
+#include "../VoiceEffectCore.h"
+#include "sst/effects/Reverb1.h"
+#include "sst/basic-blocks/params/ParamMetadata.h"
+#include "sst/basic-blocks/mechanics/block-ops.h"
+#include "FXConfigFromVFXConfig.h"
+
+namespace sst::voice_effects::liftbus
+{
+
+namespace mech = sst::basic_blocks::mechanics;
+template <typename VFXConfig> struct LiftedReverb1 : core::VoiceEffectTemplateBase<VFXConfig>
+{
+    static constexpr const char *effectName{"Reverb1"};
+
+    static constexpr int numFloatParams{8};
+    static constexpr int numIntParams{1};
+
+    using reverb1_t =
+        sst::effects::reverb1::Reverb1<FXConfigFromVFXConfig<LiftedReverb1<VFXConfig>>>;
+    LiftHelper<LiftedReverb1, reverb1_t> helper;
+
+    LiftedReverb1() : helper(this), core::VoiceEffectTemplateBase<VFXConfig>() {}
+
+    basic_blocks::params::ParamMetaData paramAt(int idx) const
+    {
+        using pmd = basic_blocks::params::ParamMetaData;
+        auto udx = idx;
+        if (idx > 0) // we skip shape
+            udx = idx + 1;
+        return helper.busFX->paramAt(udx);
+    }
+
+    basic_blocks::params::ParamMetaData intParamAt(int idx) const
+    {
+        return helper.busFX->paramAt(reverb1_t::rev1_shape);
+    }
+
+    void initVoiceEffect() { helper.init(); }
+
+    void initVoiceEffectParams() { this->initToParamMetadataDefault(this); }
+
+    size_t tailLength() const { return helper.busFX->getRingoutDecay() * VFXConfig::blockSize; }
+
+    void setupValues()
+    {
+        for (int i = 0; i < numFloatParams; ++i)
+        {
+            auto udx = i;
+            if (i > 0) // we skip shape
+                udx = i + 1;
+            helper.valuesForFX[udx] = this->getFloatParam(i);
+        }
+        helper.valuesForFX[reverb1_t::rev1_shape] = this->getIntParam(0);
+
+        helper.valuesForFX[reverb1_t::rev1_mix] = 1.0;
+        helper.valuesForFX[reverb1_t::rev1_width] = helper.busFX->getDefaultWidth();
+    }
+    void processStereo(float *datainL, float *datainR, float *dataoutL, float *dataoutR,
+                       float pitch)
+    {
+        setupValues();
+        mech::copy_from_to<VFXConfig::blockSize>(datainL, dataoutL);
+        mech::copy_from_to<VFXConfig::blockSize>(datainR, dataoutR);
+        helper.busFX->processBlock(dataoutL, dataoutR);
+    }
+};
+} // namespace sst::voice_effects::liftbus
+#endif // SHORTCIRCUITXT_LIFTEDREVERB2_H
