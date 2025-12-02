b2d9581ca353c00513a84f2f0683da55e0ecb8fa
Andreya-Autumn <andreya.ek.frisk@gmail.com>
2025-12-02 14:00:18 +0100

    done for now

--- include/sst/voice-effects/generator/3opPhaseMod.h
+++ include/sst/voice-effects/generator/3opPhaseMod.h
@@ -36,8 +36,8 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
     static constexpr int numFloatParams{9};
     static constexpr int numIntParams{0};
 
-    using PmSineTable = sst::basic_blocks::tables::SixSinesWaveProvider;
-    PmSineTable &sPmSineTable;
+    using ssSineTable = sst::basic_blocks::tables::SixSinesWaveProvider;
+    ssSineTable &sPmSineTable;
     using twoToTheXTable = sst::basic_blocks::tables::TwoToTheXProvider;
     const twoToTheXTable &sTwoToTheXTable;
 
@@ -54,7 +54,7 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
         fpLevel3,
     };
 
-    ThreeOpPhaseMod(const twoToTheXTable &tt, PmSineTable &pst)
+    ThreeOpPhaseMod(const twoToTheXTable &tt, ssSineTable &pst)
         : core::VoiceEffectTemplateBase<VFXConfig>(), sPmSineTable(pst), sTwoToTheXTable(tt)
     {
     }
@@ -68,25 +68,33 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
         switch (idx)
         {
         case fpRatio2:
-            return pmd()
-                .asFloat()
-                .withRange(-5, 5)
-                .withATwoToTheBFormatting(1, 1, "x")
-                .withName("Op 2 Ratio")
-                .withDecimalPlaces(4)
-                .withDefault(0.0)
-                .withFeature(pmd::Features::BELOW_ONE_IS_INVERSE_FRACTION)
-                .withFeature(pmd::Features::ALLOW_FRACTIONAL_TYPEINS);
+            if (keytrackOn)
+            {
+                return pmd()
+                    .asFloat()
+                    .withRange(-5, 5)
+                    .withATwoToTheBFormatting(1, 1, "x")
+                    .withName("Op 2 Ratio")
+                    .withDecimalPlaces(4)
+                    .withDefault(0.0)
+                    .withFeature(pmd::Features::BELOW_ONE_IS_INVERSE_FRACTION)
+                    .withFeature(pmd::Features::ALLOW_FRACTIONAL_TYPEINS);
+            }
+            return pmd().asAudibleFrequency().withName("Op 2 Freq");
         case fpRatio3:
-            return pmd()
-                .asFloat()
-                .withRange(-5, 5)
-                .withATwoToTheBFormatting(1, 1, "x")
-                .withName("Op 3 Ratio")
-                .withDecimalPlaces(4)
-                .withDefault(0.0)
-                .withFeature(pmd::Features::BELOW_ONE_IS_INVERSE_FRACTION)
-                .withFeature(pmd::Features::ALLOW_FRACTIONAL_TYPEINS);
+            if (keytrackOn)
+            {
+                return pmd()
+                    .asFloat()
+                    .withRange(-5, 5)
+                    .withATwoToTheBFormatting(1, 1, "x")
+                    .withName("Op 3 Ratio")
+                    .withDecimalPlaces(4)
+                    .withDefault(0.0)
+                    .withFeature(pmd::Features::BELOW_ONE_IS_INVERSE_FRACTION)
+                    .withFeature(pmd::Features::ALLOW_FRACTIONAL_TYPEINS);
+            }
+            return pmd().asAudibleFrequency().withName("Op 3 Freq");
         case fpFeedback2:
             return pmd().asFloat().asPercentBipolar().withName("Op 2 Feedback");
         case fpFeedback3:
@@ -113,56 +121,72 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
         return pmd().withName("Error");
     }
 
-    void initVoiceEffect() {}
+    void initVoiceEffect()
+    {
+        namespace mech = sst::basic_blocks::mechanics;
+
+        mech::clear_block<VFXConfig::blockSize>(freq2);
+        mech::clear_block<VFXConfig::blockSize>(freq3);
+        mech::clear_block<VFXConfig::blockSize>(fbk2);
+        mech::clear_block<VFXConfig::blockSize>(fbk3);
+        mech::clear_block<VFXConfig::blockSize>(d12);
+        mech::clear_block<VFXConfig::blockSize>(d13);
+        mech::clear_block<VFXConfig::blockSize>(d23);
+    }
     void initVoiceEffectParams() { this->initToParamMetadataDefault(this); }
 
     void processStereo(const float *const datainL, const float *const datainR, float *dataoutL,
                        float *dataoutR, float pitch)
     {
         namespace mech = sst::basic_blocks::mechanics;
-        float sumIn alignas(16)[VFXConfig::blockSize];
-        mech::add_block<VFXConfig::blockSize>(datainL, datainR, sumIn);
 
-        float sumOut alignas(16)[VFXConfig::blockSize];
-        processMonoToMono(sumIn, sumOut, pitch);
+        setupForBlock(pitch);
 
-        mech::copy_from_to<VFXConfig::blockSize>(sumOut, dataoutL);
-        mech::copy_from_to<VFXConfig::blockSize>(sumOut, dataoutR);
+        int32_t pm2 alignas(16)[2][VFXConfig::blockSize];
+        for (int i = 0; i < VFXConfig::blockSize; i++)
+        {
+            pm2[0][i] = (int32_t)((1 << 27) * (datainL[i] * d12[i]));
+            pm2[1][i] = (int32_t)((1 << 27) * (datainR[i] * d12[i]));
+        }
+
+        float out2 alignas(16)[2][VFXConfig::blockSize];
+        for (int c = 0; c < 2; c++)
+        {
+            innerLoop(out2[c], fbVal2[c], fbk2, freq2, phase2[c], pm2[c]);
+        }
+
+        int32_t pm3 alignas(16)[2][VFXConfig::blockSize];
+        for (int i = 0; i < VFXConfig::blockSize; i++)
+        {
+            pm3[0][i] = (int32_t)((1 << 27) * (datainL[i] * d13[i]));
+            pm3[0][i] += (int32_t)((1 << 27) * (out2[0][i] * d23[i]));
+
+            pm3[1][i] = (int32_t)((1 << 27) * (datainR[i] * d13[i]));
+            pm3[1][i] += (int32_t)((1 << 27) * (out2[1][i] * d23[i]));
+        }
+
+        float out3 alignas(16)[2][VFXConfig::blockSize];
+        for (int c = 0; c < 2; c++)
+        {
+            innerLoop(out3[c], fbVal3[c], fbk3, freq3, phase3[c], pm3[c]);
+        }
+
+        level2Lerp.multiply_2_blocks(out2[0], out2[1]);
+        level3Lerp.multiply_2_blocks(out3[0], out3[1]);
+
+        mech::clear_block<VFXConfig::blockSize>(dataoutL);
+        mech::clear_block<VFXConfig::blockSize>(dataoutR);
+        mech::scale_accumulate_from_to<VFXConfig::blockSize>(out2[0], out2[1], .5f, dataoutL,
+                                                             dataoutR);
+        mech::scale_accumulate_from_to<VFXConfig::blockSize>(out3[0], out3[1], .5f, dataoutL,
+                                                             dataoutR);
     }
 
     void processMonoToMono(const float *const datain, float *dataout, float pitch)
     {
         namespace mech = sst::basic_blocks::mechanics;
 
-        float freq2 alignas(16)[VFXConfig::blockSize];
-        freq2Lerp.set_target(440 * this->note_to_pitch_ignoring_tuning(pitch) *
-                             sTwoToTheXTable.twoToThe(this->getFloatParam(fpRatio2)));
-        freq2Lerp.store_block(freq2);
-
-        float freq3 alignas(16)[VFXConfig::blockSize];
-        freq3Lerp.set_target(440 * this->note_to_pitch_ignoring_tuning(pitch) *
-                             sTwoToTheXTable.twoToThe(this->getFloatParam(fpRatio3)));
-        freq3Lerp.store_block(freq3);
-
-        float fbk2 alignas(16)[VFXConfig::blockSize];
-        fbk2Lerp.set_target(this->getFloatParam(fpFeedback2));
-        fbk2Lerp.store_block(fbk2);
-
-        float fbk3 alignas(16)[VFXConfig::blockSize];
-        fbk3Lerp.set_target(this->getFloatParam(fpFeedback3));
-        fbk3Lerp.store_block(fbk3);
-
-        float d12 alignas(16)[VFXConfig::blockSize];
-        depth12Lerp.set_target(this->getFloatParam(fp1to2));
-        depth12Lerp.store_block(d12);
-
-        float d13 alignas(16)[VFXConfig::blockSize];
-        depth13Lerp.set_target(this->getFloatParam(fp1to3));
-        depth13Lerp.store_block(d13);
-
-        float d23 alignas(16)[VFXConfig::blockSize];
-        depth23Lerp.set_target(this->getFloatParam(fp2to3));
-        depth23Lerp.store_block(d23);
+        setupForBlock(pitch);
 
         int32_t pm2 alignas(16)[VFXConfig::blockSize];
         for (int i = 0; i < VFXConfig::blockSize; i++)
@@ -171,7 +195,7 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
         }
 
         float out2 alignas(16)[VFXConfig::blockSize];
-        innerLoop(out2, fbVal2, fbk2, freq2, pm2, phase2, dPhase2);
+        innerLoop(out2, fbVal2[0], fbk2, freq2, phase2[0], pm2);
 
         int32_t pm3 alignas(16)[VFXConfig::blockSize];
         for (int i = 0; i < VFXConfig::blockSize; i++)
@@ -181,15 +205,14 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
         }
 
         float out3 alignas(16)[VFXConfig::blockSize];
-        innerLoop(out3, fbVal3, fbk3, freq3, pm3, phase3, dPhase3);
+        innerLoop(out3, fbVal3[0], fbk3, freq3, phase3[0], pm3);
 
-        level2Lerp.set_target(this->getFloatParam(fpLevel2));
-        level3Lerp.set_target(this->getFloatParam(fpLevel3));
         level2Lerp.multiply_block(out2);
         level3Lerp.multiply_block(out3);
 
-        mech::add_block<VFXConfig::blockSize>(out3, out2);
-        mech::copy_from_to<VFXConfig::blockSize>(out3, dataout);
+        mech::clear_block<VFXConfig::blockSize>(dataout);
+        mech::scale_accumulate_from_to<VFXConfig::blockSize>(out2, .5f, dataout);
+        mech::scale_accumulate_from_to<VFXConfig::blockSize>(out3, .5f, dataout);
     }
 
     bool enableKeytrack(bool b)
@@ -206,35 +229,74 @@ template <typename VFXConfig> struct ThreeOpPhaseMod : core::VoiceEffectTemplate
   protected:
     bool keytrackOn{false}, wasKeytrackOn{false}, first{true};
 
+    uint32_t phase2[2]{0, 0};
+    float fbVal2[2][2]{{0.f, 0.f}, {0.f, 0.f}};
+    uint32_t phase3[2]{0, 0};
+    float fbVal3[2][2]{{0.f, 0.f}, {0.f, 0.f}};
+
+    float freq2 alignas(16)[VFXConfig::blockSize];
+    float freq3 alignas(16)[VFXConfig::blockSize];
+    float fbk2 alignas(16)[VFXConfig::blockSize];
+    float fbk3 alignas(16)[VFXConfig::blockSize];
+    float d12 alignas(16)[VFXConfig::blockSize];
+    float d13 alignas(16)[VFXConfig::blockSize];
+    float d23 alignas(16)[VFXConfig::blockSize];
+
     sst::basic_blocks::dsp::lipol_sse<VFXConfig::blockSize, true> freq2Lerp, fbk2Lerp, freq3Lerp,
         fbk3Lerp, depth12Lerp, depth13Lerp, depth23Lerp, level2Lerp, level3Lerp;
 
-    uint32_t phase2;
-    int32_t dPhase2;
-    int32_t phaseInput2;
-    float fbVal2[2]{0.f, 0.f};
+    void setupForBlock(float pitch)
+    {
+        auto fp2{220.f}, fp3{220.f};
 
-    uint32_t phase3;
-    int32_t dPhase3;
-    int32_t phaseInput3;
-    float fbVal3[2]{0.f, 0.f};
+        if (keytrackOn)
+        {
+            fp2 *= this->note_to_pitch_ignoring_tuning(pitch) *
+                   sTwoToTheXTable.twoToThe(this->getFloatParam(fpRatio2));
+            fp3 *= this->note_to_pitch_ignoring_tuning(pitch) *
+                   sTwoToTheXTable.twoToThe(this->getFloatParam(fpRatio3));
+        }
+        else
+        {
+            fp2 *= this->note_to_pitch_ignoring_tuning(this->getFloatParam(fpRatio2));
+            fp3 *= this->note_to_pitch_ignoring_tuning(this->getFloatParam(fpRatio3));
+        }
 
-    void innerLoop(float *onto, float *fbv, float const *fbl, float const *freq, int32_t *phsIn,
-                   uint32_t &phs, int32_t &dphs)
+        freq2Lerp.set_target(fp2);
+        freq3Lerp.set_target(fp3);
+        fbk2Lerp.set_target(this->getFloatParam(fpFeedback2));
+        fbk3Lerp.set_target(this->getFloatParam(fpFeedback3));
+        depth12Lerp.set_target(this->getFloatParam(fp1to2));
+        depth13Lerp.set_target(this->getFloatParam(fp1to3));
+        depth23Lerp.set_target(this->getFloatParam(fp2to3));
+        level2Lerp.set_target(this->getFloatParam(fpLevel2));
+        level3Lerp.set_target(this->getFloatParam(fpLevel3));
+
+        freq2Lerp.store_block(freq2);
+        freq3Lerp.store_block(freq3);
+        fbk2Lerp.store_block(fbk2);
+        fbk3Lerp.store_block(fbk3);
+        depth12Lerp.store_block(d12);
+        depth13Lerp.store_block(d13);
+        depth23Lerp.store_block(d23);
+    }
+
+    void innerLoop(float *output, float *fbv, float const *fbl, float const *freq, uint32_t &phs,
+                   int32_t *pmv)
     {
         for (int i = 0; i < VFXConfig::blockSize; ++i)
         {
-            dphs = sPmSineTable.dPhase(freq[i]);
+            auto dphs = sPmSineTable.dPhase(freq[i]);
             phs += dphs;
 
             auto fb = 0.5 * (fbv[0] + fbv[1]);
             auto sb = std::signbit(fbl[i]);
             fb = fb * (1 - sb * (1 - fb));
 
-            auto ph = phs + phsIn[i] + static_cast<int32_t>((1 << 24) * (fbl[i] * fb));
+            auto ph = phs + pmv[i] + static_cast<int32_t>((1 << 24) * (fbl[i] * fb));
             auto out = sPmSineTable.at(ph);
 
-            onto[i] = out;
+            output[i] = out;
             fbv[1] = fbv[0];
             fbv[0] = out;
         }
